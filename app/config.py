fc3303910ed0e15653aabe98151ffe04fa0c9b25
lemarodrigo <lemarodrigo@gmail.com>
2024-07-31 16:41:10 -0300

    actualizo OS getenv

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@
 import os
 
 class Config:
-    API_BASE_URL = 'http://localhost:5000' # 'https://api.example.com'
-    APP_TITLE = 'REQUIEM Panel'
-    SESSION_COOKIE_SAMESITE="None"
-    SESSION_COOKIE_SECURE=True
+    API_BASE_URL = os.getenv('API_BASE_URL', 'http://localhost:5000')
+    APP_TITLE = os.getenv('APP_TITLE', 'REQUIEM Panel')
+    SESSION_COOKIE_SAMESITE=os.getenv('SESSION_COOKIE_SAMESITE', 'None')
+    SESSION_COOKIE_SECURE=os.getenv('SESSION_COOKIE_SECURE', 'True')
